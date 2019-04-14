f7dfc1dc56a5c4254b5c6e9531c3d94ae5b6c053
Yann Stepienik <azukaar@gmail.com>
2019-04-14 11:00:00 +0100

    wip

--- src/index.go
+++ src/index.go
@@ -1,17 +1,53 @@
 package main
 
 import (
-	"github.com/robertkrimen/otto"
+	"os"
+	// "github.com/robertkrimen/otto"
+	"github.com/spf13/cobra"
 	"fmt"
 	"time"
 )
 
+var Provider string
+
+var installCmd = &cobra.Command{
+	Use:   "install [--provider=]",
+	Short: "Install package",
+	Long:  `Install package based on info in the entry point (depends on provider)`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if(Provider != "") {
+			fmt.Println("Reading provider config for", Provider);
+			providerJson := readJSON("plugins/provider-" + Provider + "/gupm.json")
+			fmt.Println("Initialisation OK for", Provider);
+			fmt.Println(providerJson["config"])
+		}
+	},
+}
+  
+  var rootCmd = &cobra.Command{
+	Use:   "hugo",
+	Short: "Hugo is a very fast static site generator",
+	Long: `A Fast and Flexible Static Site Generator built with
+				  love by spf13 and friends in Go.
+				  Complete documentation is available at http://hugo.spf13.com`,
+	Run: func(cmd *cobra.Command, args []string) {
+	  fmt.Println("test")
+	},
+  }
+  
+  func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+	  fmt.Println(err)
+	  os.Exit(1)
+	}
+  }
+
 func main() {
 	start := time.Now()
-	vm := otto.New()
-	vm.Run(`
-		abc = 2 + 2;
-		console.log("The value of abc is " + abc); // 4
-	`)
+
+	rootCmd.AddCommand(installCmd)
+	installCmd.PersistentFlags().StringVarP(&Provider, "provider", "p", "", "Provider plugin")
+
+	Execute()
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
