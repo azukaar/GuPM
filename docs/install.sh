817ce167ed7171fd363fc46fe4f947cac9e798c4
Yann Stepienik <azukaar@gmail.com>
2019-04-23 18:30:57 +0100

    Add script

---
+++ docs/install.sh
@@ -0,0 +1,8 @@
+wget https://azukaar.github.io/GuPM/gupm.tar.gz
+sudo tar -C /etc/ -zxvf gupm.tar.gz
+rm gupm.tar.gz
+
+echo "Installaton complete"
+echo "------"
+echo "Add /etc/gupm to your PATH to use GuPM"
+echo "------"
--- make.prod.sh
+++ make.prod.sh
@@ -3,6 +3,7 @@ rm -rf docs/
 mkdir docs
 go build -o gupm/gupm src/*.go
 cp -R plugins gupm/plugins
+sed -i 's/\r//g' install.sh
 cp -R install.sh docs/install.sh
 tar czf docs/gupm.tar.gz gupm
 rm -rf gupm/
