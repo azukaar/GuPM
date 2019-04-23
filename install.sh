02aa199d482574d9bc9fefa13d6719c3900279bf
Yann Stepienik <azukaar@gmail.com>
2019-04-23 19:50:20 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-wget https://azukaar.github.io/GuPM/gupm.tar.gz
-sudo tar -C /etc/ -zxvf gupm.tar.gz
+curl --output gupm.tar.gz https://azukaar.github.io/GuPM/gupm.tar.gz
+su
+tar -C /etc/ -zxvf gupm.tar.gz
 rm gupm.tar.gz
 
 echo "------"
