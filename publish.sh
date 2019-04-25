f53758850206adb2836b4e3483bfaaa361bf9f50
Yann Stepienik <azukaar@gmail.com>
2019-04-25 14:48:58 +0100

    publish

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@ rm -rf docs/
 mkdir docs
 go build -o gupm/g src/*.go
 cp -R plugins gupm/plugins
+cp gupm.json gupm/gupm.json
 sed -i 's/\r//g' install.sh
 cp -R install.sh docs/install.sh
 tar czf docs/gupm.tar.gz gupm
