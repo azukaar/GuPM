e06dc3a174d0cb7ba289cbec9028713cd26c0955
Yann Stepienik <azukaar@gmail.com>
2019-05-17 21:44:34 +0100

    cleanup

--- build.sh
+++
@@ -1,4 +0,0 @@
-rm -rf build/dg build/plugins build/gupm.json
-go build -o build/dg src/index.go src/addDependency.go src/installProject.go
-cp -R plugins build/plugins
-cp gupm.json build/gupm.json
