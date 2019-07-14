014f0393ba28a62d7df0b4a3c12887106d439bc4
Yann Stepienik <azukaar@gmail.com>
2019-07-14 16:34:19 +0100

    [skip ci] Release

--- src/installProject.go
+++ src/installProject.go
@@ -1,53 +1,54 @@
 package main
 
 import (
-	"./provider"
 	"errors"
-	"./utils"
-	"./ui"
-	"sync"
 	"fmt"
+	"sync"
 	"time"
+
+	"./provider"
+	"./ui"
+	"./utils"
 	"github.com/mitchellh/go-homedir"
 )
 
-var cacheExpanded = make(map[string]map[string]interface {})
+var cacheExpanded = make(map[string]map[string]interface{})
 var lock = sync.RWMutex{}
 var lockList = sync.RWMutex{}
 
-func expandDepList(depList []map[string]interface {}) ([]map[string]interface {}) {
+func expandDepList(depList []map[string]interface{}) []map[string]interface{} {
 	channel := make(chan int)
-	
+
 	for index, dep := range depList {
-		go (func(channel chan int, index int, dep map[string]interface {}) {
-			if(dep["expanded"] != true) {
-				newDep := make(map[string]interface {})
+		go (func(channel chan int, index int, dep map[string]interface{}) {
+			if dep["expanded"] != true {
+				newDep := make(map[string]interface{})
 				for key, value := range dep {
 					newDep[key] = value
 				}
 				newDep, errExpand := provider.ResolveDependencyLocation(newDep)
-				if(newDep == nil) {
+				if newDep == nil {
 					ui.Error("Provider " + dep["provider"].(string) + " didnt resolve " + dep["name"].(string) + "@" + dep["version"].(string))
 					ui.Error(errExpand)
 					channel <- 0
-					return;
+					return
 				}
 
 				hdir, errH := homedir.Dir()
-				if(errH != nil) {
+				if errH != nil {
 					ui.Error(errH)
 					hdir = "."
 				}
 				_, ok := newDep["url"].(string)
-				if(!ok || newDep["url"].(string) == "") {
+				if !ok || newDep["url"].(string) == "" {
 					ui.Error("Cannot resolve : " + newDep["name"].(string))
 					channel <- 1
-					return;
+					return
 				}
 
 				newDep["path"] = hdir + "/.gupm/cache/" + newDep["provider"].(string) + "/" + newDep["name"].(string) + "/" + newDep["version"].(string)
 
-				if(!utils.FileExists(newDep["path"].(string)) || !utils.FileExists(newDep["path"].(string) + "/.gupm_locked")) {
+				if !utils.FileExists(newDep["path"].(string)) || !utils.FileExists(newDep["path"].(string)+"/.gupm_locked") {
 					getRes, errorGD := provider.GetDependency(
 						newDep["provider"].(string),
 						newDep["name"].(string),
@@ -55,7 +56,7 @@ func expandDepList(depList []map[string]interface {}) ([]map[string]interface {}
 						newDep["url"].(string),
 						newDep["path"].(string),
 					)
-					if(errorGD != nil) {
+					if errorGD != nil {
 						ui.Error(errorGD)
 					}
 					_, errorPGD := provider.PostGetDependency(
@@ -66,22 +67,22 @@ func expandDepList(depList []map[string]interface {}) ([]map[string]interface {}
 						newDep["path"].(string),
 						getRes,
 					)
-					if(errorPGD != nil) {
+					if errorPGD != nil {
 						ui.Error(errorPGD)
 					}
 				}
 
 				lock.Lock()
-				if(newDep["expanded"] != true) {
-					if(cacheExpanded[newDep["url"].(string)]["expanded"] != true) {
+				if newDep["expanded"] != true {
+					if cacheExpanded[newDep["url"].(string)]["expanded"] != true {
 						newDep, errExpand = provider.ExpandDependency(newDep)
-						if(errExpand != nil || newDep == nil || len(newDep) == 0)  {
+						if errExpand != nil || newDep == nil || len(newDep) == 0 {
 							ui.Error("Provider " + dep["provider"].(string) + " didnt expand " + dep["name"].(string) + "@" + dep["version"].(string))
 							ui.Error(errExpand)
 							channel <- 0
-							return;
+							return
 						}
-		
+
 						newDep["expanded"] = true
 						cacheExpanded[newDep["url"].(string)] = newDep
 					} else {
@@ -90,14 +91,14 @@ func expandDepList(depList []map[string]interface {}) ([]map[string]interface {}
 				}
 				ui.Log("Get dependency " + newDep["name"].(string))
 				lock.Unlock()
-				
+
 				lockList.Lock()
 
 				depList[index] = newDep
-				nextDepList, ok := depList[index]["dependencies"].([]map[string]interface {})
+				nextDepList, ok := depList[index]["dependencies"].([]map[string]interface{})
 				lockList.Unlock()
 
-				if(ok) {
+				if ok {
 					res := expandDepList(nextDepList)
 					lockList.Lock()
 					depList[index]["dependencies"] = res
@@ -108,9 +109,9 @@ func expandDepList(depList []map[string]interface {}) ([]map[string]interface {}
 		})(channel, index, dep)
 	}
 
-	for _,_ = range depList {
+	for _, _ = range depList {
 		i := <-channel
-		if(i == 0) {
+		if i == 0 {
 			return nil
 		}
 	}
@@ -118,19 +119,19 @@ func expandDepList(depList []map[string]interface {}) ([]map[string]interface {}
 	return depList
 }
 
-func installDep(path string, depList []map[string]interface {}) map[string]string {
+func installDep(path string, depList []map[string]interface{}) map[string]string {
 	installPaths := make(map[string]string)
 	installPathsLock := sync.RWMutex{}
 
 	var channel = make(chan int)
 	for index, dep := range depList {
-		go (func(channel chan int, index int, dep map[string]interface {}){
+		go (func(channel chan int, index int, dep map[string]interface{}) {
 			depProviderConfig, err := provider.GetProviderConfig(dep["provider"].(string))
 			destination := utils.Path(path + "/" + depProviderConfig.Config.Default.InstallPath)
 
 			if dep["path"] != nil {
 				packageConfig, errC := utils.ReadGupmJson(utils.Path(dep["path"].(string) + "/gupm.json"))
-				
+
 				if errC == nil && packageConfig != nil && packageConfig.WrapInstallFolder != "" {
 					destination += utils.Path("/" + packageConfig.WrapInstallFolder)
 				}
@@ -140,21 +141,21 @@ func installDep(path string, depList []map[string]interface {}) map[string]strin
 			ui.Log("Installing " + path)
 			provider.InstallDependency(destination, dep)
 
-			if(path == ".") {
+			if path == "." {
 				installPathsLock.Lock()
 				installPaths[dep["provider"].(string)] = depProviderConfig.Config.Default.InstallPath
 				installPathsLock.Unlock()
 			}
 
-			nextDepList, ok := depList[index]["dependencies"].([]map[string]interface {})
+			nextDepList, ok := depList[index]["dependencies"].([]map[string]interface{})
 
-			if(ok) {
-				installDep(utils.Path(destination + "/" + depList[index]["name"].(string)), nextDepList)
+			if ok {
+				installDep(utils.Path(destination+"/"+depList[index]["name"].(string)), nextDepList)
 			}
 			channel <- 1
 		})(channel, index, dep)
 	}
-	for _,_ = range depList {
+	for _, _ = range depList {
 		<-channel
 	}
 	return installPaths
@@ -166,13 +167,13 @@ func InstallProject(path string) error {
 	start := time.Now()
 
 	ui.Title("Installing project...")
-	
+
 	var err error
 	var packageConfig utils.Json
-	var depList []map[string]interface {}
+	var depList []map[string]interface{}
 
 	err = provider.InitProvider(Provider)
-	if(err != nil) {
+	if err != nil {
 		return err
 	}
 
@@ -182,23 +183,23 @@ func InstallProject(path string) error {
 	packageConfig, _ = provider.PostGetPackageConfig(packageConfig)
 
 	depList, err = provider.GetDependencyList(packageConfig)
-	if(err != nil) {
+	if err != nil {
 		return err
 	}
-	if(depList == nil) {
+	if depList == nil {
 		ui.Log("No dependencies found.")
 		return nil
 	}
 
 	ui.Title("Expand dependency list...")
 	depList = expandDepList(depList)
-	if(depList == nil) {
+	if depList == nil {
 		return errors.New("Failed to expand dependancy list")
 	}
-	
+
 	ui.Title("Build dependency list...")
 	depList = provider.BuildDependencyTree(depList)
-	if(depList == nil) {
+	if depList == nil {
 		return errors.New("Failed to build dependancy list")
 	}
 
@@ -207,13 +208,13 @@ func InstallProject(path string) error {
 
 	ui.Title("Install Binaries...")
 	err = provider.BinaryInstall(installPaths)
-	if(err != nil) {
+	if err != nil {
 		return err
 	}
 
 	ui.Title("Installation done ❤️")
 
 	timeElapsed := fmt.Sprintf("%f", time.Since(start).Seconds())
-	ui.Log(timeElapsed+"s elapsed\n")
+	ui.Log(timeElapsed + "s elapsed\n")
 	return nil
 }
